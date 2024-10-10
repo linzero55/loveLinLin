325fb47cd3e0e35e29791b8ef0e4896124603b77
linzewu <linzewu@bigo.sg>
2024-10-10 15:48:09 +0800

    修改

--- js/lovetime.js
+++ js/lovetime.js
@@ -13,7 +13,7 @@ function lovetime() {
     var todayMinute = today.getMinutes()
     var todaySecond = today.getSeconds()
     // 修改这里时间即可 我的是2021.05.04
-    var t1 = Date.UTC(2024, 07, 24, 00, 00, 00)
+    var t1 = new Date('2024-07-24T12:00:00');
     var t2 = Date.UTC(todayYear, todayMonth, todayDate, todayHour, todayMinute, todaySecond)
     var diff = t2 - t1
     var diffYears = Math.floor(diff / years)
